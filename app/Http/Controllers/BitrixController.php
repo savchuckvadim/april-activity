5a1b9d8b5a2ec0a9becf1845113274fbfe0a4c49
savchuckvadim <savchuckvadim@gmail.com>
2023-08-21 18:19:43 +0200

    testing btrx

--- app/Http/Controllers/BitrixController.php
+++ app/Http/Controllers/BitrixController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use CRest;
 use Illuminate\Http\Request;
+use Vipblogger\LaravelBitrix24\Bitrix;
 
 class BitrixController extends Controller
 {
@@ -18,8 +19,9 @@ class BitrixController extends Controller
         // define('C_REST_WEB_HOOK_URL', 'https://' . $domain . '/rest/1/' . $hoook); //url on creat Webhook
         define('C_REST_CLIENT_ID', 'local.64e384619d5aa1.57865711'); //url on creat Webhook
         define('C_REST_CLIENT_SECRET', '62sEXPgn6xOhGHKBunzrcEmatBw9J3irdW9kXIoSfruHTnpLPx'); //url on creat Webhook
-        $check = CRest::checkServer();
-        $profile = CRest::call('profile');
+        $bitrix = new Bitrix ;
+        
+        $profile = $bitrix->call('profile');
         return $profile ;
     }
 }
