a51f51e13d7e35252b346b780109a19abd07f9da
savchuckvadim <savchuckvadim@gmail.com>
2023-08-21 17:54:18 +0200

    fxd Crest error

--- app/Http/Controllers/BitrixController.php
+++ app/Http/Controllers/BitrixController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Bitrix24api\CRest as Bitrix24apiCRest;
-// use CRest;
+use CRest;
 use Illuminate\Http\Request;
 
 class BitrixController extends Controller
@@ -19,8 +18,8 @@ class BitrixController extends Controller
         // define('C_REST_WEB_HOOK_URL', 'https://' . $domain . '/rest/1/' . $hoook); //url on creat Webhook
         define('C_REST_CLIENT_ID', 'local.64e384619d5aa1.57865711'); //url on creat Webhook
         define('C_REST_CLIENT_SECRET', '62sEXPgn6xOhGHKBunzrcEmatBw9J3irdW9kXIoSfruHTnpLPx'); //url on creat Webhook
-        $check = Bitrix24apiCRest::checkServer();
-        $profile = Bitrix24apiCRest::call('profile');
+        $check = CRest::checkServer();
+        $profile = CRest::call('profile');
         return $profile ;
     }
 }
