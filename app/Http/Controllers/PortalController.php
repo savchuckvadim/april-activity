da5b7e4f5f1e367bfe08553290513d6169e8870f
savchuckvadim <savchuckvadim@gmail.com>
2023-12-21 10:30:40 +0100

    testing set protal

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -10,76 +10,85 @@ class PortalController extends Controller
 {
     public static function setPortal($number, $domain, $key, $clientId, $secret, $hook)
     {
-        $data = [
-            'number' => $number,
-            'domain' => $domain,
-            'key' => $key,
-            'C_REST_CLIENT_ID' => $clientId,
-            'C_REST_CLIENT_SECRET' => $secret,
-            'C_REST_WEB_HOOK_URL' => $hook,
-        ];
-        if (empty($domain) || empty($key) || empty($clientId) || empty($secret) || empty($hook)) {
+        try {
+            $data = [
+                'number' => $number,
+                'domain' => $domain,
+                'key' => $key,
+                'C_REST_CLIENT_ID' => $clientId,
+                'C_REST_CLIENT_SECRET' => $secret,
+                'C_REST_WEB_HOOK_URL' => $hook,
+            ];
+            if (empty($domain) || empty($key) || empty($clientId) || empty($secret) || empty($hook)) {
+                return response([
+                    'resultCode' => 1, 'message' => "invalid data",
+                    'data' => [
+                        'number' => $number,
+                        'domain' => $domain,
+                        'key' => $key,
+                        'C_REST_CLIENT_ID' => $clientId,
+                        'C_REST_CLIENT_SECRET' => $secret,
+                        'C_REST_WEB_HOOK_URL' => $hook,
+                    ]
+                ]);
+            } 
+    
+            $data['key'] = Crypt::encryptString($key);
+            $data['C_REST_CLIENT_ID'] = Crypt::encryptString($clientId);
+            $data['C_REST_CLIENT_SECRET']  = Crypt::encryptString($secret);
+            $data['C_REST_WEB_HOOK_URL'] =   Crypt::encryptString($hook);
+    
+            $portal = Portal::firstOrCreate(
+                ['domain' => $domain], // Условия для поиска
+                $data // Значения по умолчанию, если создается новая запись
+            );
+            // $portal->save();
+            // return response([
+            //     'resultCode' => 0,
+            //     'message' => 'success',
+            //     'portal' => [
+            //         'id' => $portal->id,
+            //         'number' => $portal->number,
+            //         'domain' => $domain,
+            //         // 'key' => $portal->getKey(),
+            //         // 'C_REST_CLIENT_ID' => $portal->getClientId(),
+            //         // 'C_REST_CLIENT_SECRET' => $portal->getSecret(),
+            //         // 'C_REST_WEB_HOOK_URL' => $portal->getHook(),
+            //     ]
+    
+            // ]);
+    
+    
+    
+    
+    
+            $portal->save();
+    
+    
+    
             return response([
-                'resultCode' => 1, 'message' => "invalid data",
-                'data' => [
-                    'number' => $number,
+                'resultCode' => 0,
+                'message' => 'success',
+                'portal' => [
+                    'id' => $portal->id,
+                    'number' => $portal->number,
                     'domain' => $domain,
-                    'key' => $key,
-                    'C_REST_CLIENT_ID' => $clientId,
-                    'C_REST_CLIENT_SECRET' => $secret,
-                    'C_REST_WEB_HOOK_URL' => $hook,
+                    'key' => $portal->key,
+                    'C_REST_CLIENT_ID' => $portal->C_REST_CLIENT_ID,
+                    'C_REST_CLIENT_SECRET' => $portal->C_REST_CLIENT_SECRET,
+                    'C_REST_WEB_HOOK_URL' => $portal->C_REST_WEB_HOOK_URL,
                 ]
+    
             ]);
-        } else {
+        } catch (\Throwable $th) {
+            return  response([
+                'resultCode' => 0,
+                'message' => 'error',
+                '$data' => $data
+    
+            ]);
         }
-
-        $data['key'] = Crypt::encryptString($key);
-        $data['C_REST_CLIENT_ID'] = Crypt::encryptString($clientId);
-        $data['C_REST_CLIENT_SECRET']  = Crypt::encryptString($secret);
-        $data['C_REST_WEB_HOOK_URL'] =   Crypt::encryptString($hook);
-
-        $portal = Portal::firstOrCreate(
-            ['domain' => $domain], // Условия для поиска
-            $data // Значения по умолчанию, если создается новая запись
-        );
-        $portal->save();
-        return response([
-            'resultCode' => 0,
-            'message' => 'success',
-            'portal' => [
-                'id' => $portal->id,
-                'number' => $portal->number,
-                'domain' => $domain,
-                // 'key' => $portal->getKey(),
-                // 'C_REST_CLIENT_ID' => $portal->getClientId(),
-                // 'C_REST_CLIENT_SECRET' => $portal->getSecret(),
-                // 'C_REST_WEB_HOOK_URL' => $portal->getHook(),
-            ]
-
-        ]);
-
-
-
-
-
-        $portal->save();
-
-
-
-        return response([
-            'resultCode' => 0,
-            'message' => 'success',
-            'portal' => [
-                'id' => $portal->id,
-                'number' => $portal->number,
-                'domain' => $domain,
-                'key' => $portal->key,
-                'C_REST_CLIENT_ID' => $portal->C_REST_CLIENT_ID,
-                'C_REST_CLIENT_SECRET' => $portal->C_REST_CLIENT_SECRET,
-                'C_REST_WEB_HOOK_URL' => $portal->C_REST_WEB_HOOK_URL,
-            ]
-
-        ]);
+        
     }
     public static function getPortal($domain)
     {
