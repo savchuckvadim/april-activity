f22434ec1c0a18c00d32120bf4191cc0bb9944be
savchuckvadim <savchuckvadim@gmail.com>
2023-12-22 11:50:49 +0100

    fxd init

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -93,19 +93,20 @@ class Template extends Model
                 'type' => 'template',
                 'isCanAddField' => true,
                 'isCanDeleteField' => true,
-                'fields' =>
+                'fields' => [
 
-                [
-                    'title' => 'Поле шаблона',
-                    'name' => 'field',
-                    'apiName' => 'field',
-                    'type' =>  'entity',   //имеет возможность создавать модели и связывать их с создаваемой сущностью
-                    'validation' => '',
-                    'initialValue' => Field::getForm(),
-                    'isCanAddField' => true,
-                    'isRequired' => true, //хотя бы одно поле в шаблоне должно быть
+                    [
+                        'title' => 'Поле шаблона',
+                        'name' => 'field',
+                        'apiName' => 'field',
+                        'type' =>  'entity',   //имеет возможность создавать модели и связывать их с создаваемой сущностью
+                        'validation' => '',
+                        'initialValue' => Field::getForm(),
+                        'isCanAddField' => true,
+                        'isRequired' => true, //хотя бы одно поле в шаблоне должно быть
 
-                ],
+                    ],
+                ]
 
             ],
             [
