e8a196629cbc9fd5956e12cef670ae174249b565
savchuckvadim <savchuckvadim@gmail.com>
2024-01-10 08:00:06 +0100

    tst value

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -6,6 +6,7 @@ use App\Models\Infoblock;
 use Ramsey\Uuid\Uuid;
 use PhpOffice\PhpWord\Shared\Converter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class DocumentController extends Controller
 {
@@ -157,44 +158,44 @@ class DocumentController extends Controller
             $count = 0;
             foreach ($complect as $group) {
                 // $table->addCell($contentWidth, $fancyTableCellStyle)->addText($group['groupsName'], $headingStyle);
-
-                foreach ($group['value'] as $infoblock) {
-                    if (array_key_exists('code', $infoblock)) {
-                        $currentInfoblock = Infoblock::where('code', $infoblock['code'])->first();
+                Log::info('LOG', ['value' => $group['value']]);
+                // foreach ($group['value'] as $infoblock) {
+                //     if (array_key_exists('code', $infoblock)) {
+                //         $currentInfoblock = Infoblock::where('code', $infoblock['code'])->first();
                         
-                        if ($currentInfoblock) {
-                            // $table->addRow(90);
-                            // $cell = $table->addRow(90)->addCell();
-                            // $cell = $table->addCell($contentWidth, $fancyTableCellStyle);
-                            // $cell->addText($group['groupsName'], $headingStyle);
-                            // $cell->addText($currentInfoblock['name'], $textStyleBold);
-                            if ($descriptionMode === 0) {
-                                // $table->addRow(90);
-                                $cell =  $table->addRow(90)->addCell($fancyTableCellStyle);
-                                $cell->addText($currentInfoblock['name'], $headingStyle);
-                            } else   if ($descriptionMode === 1) {
-                                // $table->addRow(90);
-                                $cell =  $table->addRow(90)->addCell($fancyTableCellStyle);
-                                $cell->addText($currentInfoblock['name'], $headingStyle);
-                                $cell->addText($currentInfoblock['shortDescription'], $textStyle);
-                            }
-                            // else   if ($descriptionMode === 2) {
-                            //     $table->addRow(90);
-                            //     $cell =  $table->addCell($contentWidth, $fancyTableCellStyle);
-                            //     $cell->addText($currentInfoblock['name'], $headingStyle);
-                            //     $cell->addText($currentInfoblock['descriptionForSale'], $textStyle);
-                            // } else   if ($descriptionMode === 3) {
-                            //     $table->addRow(90);
-                            //     $cell =  $table->addCell($contentWidth, $fancyTableCellStyle);
-                            //     $cell->addText($currentInfoblock['name'], $headingStyle);
-                            //     $cell->addText($currentInfoblock['descriptionForSale'], $textStyle);
-                            // }
+                //         if ($currentInfoblock) {
+                //             // $table->addRow(90);
+                //             // $cell = $table->addRow(90)->addCell();
+                //             // $cell = $table->addCell($contentWidth, $fancyTableCellStyle);
+                //             // $cell->addText($group['groupsName'], $headingStyle);
+                //             // $cell->addText($currentInfoblock['name'], $textStyleBold);
+                //             if ($descriptionMode === 0) {
+                //                 // $table->addRow(90);
+                //                 $cell =  $table->addRow(90)->addCell($fancyTableCellStyle);
+                //                 $cell->addText($currentInfoblock['name'], $headingStyle);
+                //             } else   if ($descriptionMode === 1) {
+                //                 // $table->addRow(90);
+                //                 $cell =  $table->addRow(90)->addCell($fancyTableCellStyle);
+                //                 $cell->addText($currentInfoblock['name'], $headingStyle);
+                //                 $cell->addText($currentInfoblock['shortDescription'], $textStyle);
+                //             }
+                //             // else   if ($descriptionMode === 2) {
+                //             //     $table->addRow(90);
+                //             //     $cell =  $table->addCell($contentWidth, $fancyTableCellStyle);
+                //             //     $cell->addText($currentInfoblock['name'], $headingStyle);
+                //             //     $cell->addText($currentInfoblock['descriptionForSale'], $textStyle);
+                //             // } else   if ($descriptionMode === 3) {
+                //             //     $table->addRow(90);
+                //             //     $cell =  $table->addCell($contentWidth, $fancyTableCellStyle);
+                //             //     $cell->addText($currentInfoblock['name'], $headingStyle);
+                //             //     $cell->addText($currentInfoblock['descriptionForSale'], $textStyle);
+                //             // }
 
-                            $section->addTextBreak(1);
-                            $count = $count  + 1;
-                        }
-                    }
-                }
+                //             $section->addTextBreak(1);
+                //             $count = $count  + 1;
+                //         }
+                //     }
+                // }
             }
         } else if ($styleMode == 'tableWithGroup') {
         }
