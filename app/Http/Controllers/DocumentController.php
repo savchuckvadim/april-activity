2708241b1a67d919f712a613971e81ed71e6dcf1
savchuckvadim <savchuckvadim@gmail.com>
2024-01-10 07:51:43 +0100

    tst tbl

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -108,7 +108,7 @@ class DocumentController extends Controller
         $descriptionMode = $infoblocksOptions['description']['id'];
         $styleMode = $infoblocksOptions['style'];
 
-        throw new \Exception("Невозможно записать в каталог: $styleMode");
+       
         if ($styleMode == 'list') {
             foreach ($complect as $group) {
 
@@ -137,6 +137,7 @@ class DocumentController extends Controller
                 }
             }
         } else if ($styleMode == 'table') {
+
             $fancyTableStyleName = 'Информационное наполнение';
             $fancyTableStyle = ['borderSize' => 0, 'borderColor' => 'FFFFF', 'cellMargin' => 25];
             // 'alignment' => \PhpOffice\PhpWord\SimpleType\JcTable::CENTER
@@ -154,47 +155,47 @@ class DocumentController extends Controller
             // $marginLeft = $sectionStyle->getMarginLeft();
             // $contentWidth = $fullWidth - $marginLeft - $marginRight;
             $count = 0;
-            // foreach ($complect as $group) {
-            //     // $table->addCell($contentWidth, $fancyTableCellStyle)->addText($group['groupsName'], $headingStyle);
+            foreach ($complect as $group) {
+                // $table->addCell($contentWidth, $fancyTableCellStyle)->addText($group['groupsName'], $headingStyle);
 
-            //     foreach ($group['value'] as $infoblock) {
-            //         if (array_key_exists('code', $infoblock)) {
-            //             $currentInfoblock = Infoblock::where('code', $infoblock['code'])->first();
+                foreach ($group['value'] as $infoblock) {
+                    if (array_key_exists('code', $infoblock)) {
+                        $currentInfoblock = Infoblock::where('code', $infoblock['code'])->first();
                         
-            //             if ($currentInfoblock) {
-            //                 // $table->addRow(90);
-            //                 // $cell = $table->addRow(90)->addCell();
-            //                 // $cell = $table->addCell($contentWidth, $fancyTableCellStyle);
-            //                 // $cell->addText($group['groupsName'], $headingStyle);
-            //                 // $cell->addText($currentInfoblock['name'], $textStyleBold);
-            //                 if ($descriptionMode === 0) {
-            //                     // $table->addRow(90);
-            //                     $cell =  $table->addRow(90)->addCell($fancyTableCellStyle);
-            //                     $cell->addText($currentInfoblock['name'], $headingStyle);
-            //                 } else   if ($descriptionMode === 1) {
-            //                     // $table->addRow(90);
-            //                     $cell =  $table->addRow(90)->addCell($fancyTableCellStyle);
-            //                     $cell->addText($currentInfoblock['name'], $headingStyle);
-            //                     $cell->addText($currentInfoblock['shortDescription'], $textStyle);
-            //                 }
-            //                 // else   if ($descriptionMode === 2) {
-            //                 //     $table->addRow(90);
-            //                 //     $cell =  $table->addCell($contentWidth, $fancyTableCellStyle);
-            //                 //     $cell->addText($currentInfoblock['name'], $headingStyle);
-            //                 //     $cell->addText($currentInfoblock['descriptionForSale'], $textStyle);
-            //                 // } else   if ($descriptionMode === 3) {
-            //                 //     $table->addRow(90);
-            //                 //     $cell =  $table->addCell($contentWidth, $fancyTableCellStyle);
-            //                 //     $cell->addText($currentInfoblock['name'], $headingStyle);
-            //                 //     $cell->addText($currentInfoblock['descriptionForSale'], $textStyle);
-            //                 // }
+                        if ($currentInfoblock) {
+                            // $table->addRow(90);
+                            // $cell = $table->addRow(90)->addCell();
+                            // $cell = $table->addCell($contentWidth, $fancyTableCellStyle);
+                            // $cell->addText($group['groupsName'], $headingStyle);
+                            // $cell->addText($currentInfoblock['name'], $textStyleBold);
+                            if ($descriptionMode === 0) {
+                                // $table->addRow(90);
+                                $cell =  $table->addRow(90)->addCell($fancyTableCellStyle);
+                                $cell->addText($currentInfoblock['name'], $headingStyle);
+                            } else   if ($descriptionMode === 1) {
+                                // $table->addRow(90);
+                                $cell =  $table->addRow(90)->addCell($fancyTableCellStyle);
+                                $cell->addText($currentInfoblock['name'], $headingStyle);
+                                $cell->addText($currentInfoblock['shortDescription'], $textStyle);
+                            }
+                            // else   if ($descriptionMode === 2) {
+                            //     $table->addRow(90);
+                            //     $cell =  $table->addCell($contentWidth, $fancyTableCellStyle);
+                            //     $cell->addText($currentInfoblock['name'], $headingStyle);
+                            //     $cell->addText($currentInfoblock['descriptionForSale'], $textStyle);
+                            // } else   if ($descriptionMode === 3) {
+                            //     $table->addRow(90);
+                            //     $cell =  $table->addCell($contentWidth, $fancyTableCellStyle);
+                            //     $cell->addText($currentInfoblock['name'], $headingStyle);
+                            //     $cell->addText($currentInfoblock['descriptionForSale'], $textStyle);
+                            // }
 
-            //                 $section->addTextBreak(1);
-            //                 $count = $count  + 1;
-            //             }
-            //         }
-            //     }
-            // }
+                            $section->addTextBreak(1);
+                            $count = $count  + 1;
+                        }
+                    }
+                }
+            }
         } else if ($styleMode == 'tableWithGroup') {
         }
 
