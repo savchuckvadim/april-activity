721d6ca7dd24913dfb99b175d0c9064d4460c4d7
savchuckvadim <savchuckvadim@gmail.com>
2024-01-19 13:28:57 +0100

    tst with user Id

--- app/Http/Controllers/BitrixController.php
+++ app/Http/Controllers/BitrixController.php
@@ -51,7 +51,7 @@ class BitrixController extends Controller
                                     if ($duration) {
                                         $data =   [
                                             "FILTER" => [
-                                                // "USER_ID" => $userId,
+                                                "USER_ID" => $userId,
                                                 ">CALL_DURATION" => $duration,
                                                 ">CALL_START_DATE" => $callStartDateFrom,
                                                 "<CALL_START_DATE" =>  $callStartDateTo
@@ -59,7 +59,7 @@ class BitrixController extends Controller
                                         ];
                                     } else {
                                         $data =  ["FILTER" => [
-                                            // "USER_ID" => $userId,
+                                            "USER_ID" => $userId,
                                             ">CALL_START_DATE" => $callStartDateFrom,
                                             "<CALL_START_DATE" =>  $callStartDateTo
                                         ]];
