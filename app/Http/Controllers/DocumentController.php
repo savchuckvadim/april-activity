8fbda64d55b00d7ece70f36bf1d35f0b00254c4e
savchuckvadim <savchuckvadim@gmail.com>
2024-01-24 11:56:58 +0100

    tst width

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -1605,11 +1605,11 @@ class DocumentController extends Controller
             $paragraphTitleStyle  = [...$paragraphs['head'], ...$paragraphs['align']['center']];
             $textStyle = $fonts['text']['normal'];
             $titleStyle = $fonts['text']['bold'];
-            $invoiceHeaderCellWidthFirst = $fullWidth  * 0.45;
+            $invoiceHeaderCellWidthFirst = $fullWidth  * 0.48;
             $invoiceHeaderCellWidthFirstInner = $invoiceHeaderCellWidthFirst - 30;
-            $invoiceHeaderCellWidthSecond = $fullWidth  * 0.05;
-            $invoiceHeaderCellWidthSecondInner = $invoiceHeaderCellWidthSecond - 130;
-            $invoiceHeaderCellWidthThird = $fullWidth  * 0.35;
+            $invoiceHeaderCellWidthSecond = $fullWidth  * 0.07;
+            $invoiceHeaderCellWidthSecondInner = $invoiceHeaderCellWidthSecond - 30;
+            $invoiceHeaderCellWidthThird = $fullWidth  * 0.42;
             $invoiceHeaderCellWidthThirdInner = $invoiceHeaderCellWidthThird - 30;
 
 
