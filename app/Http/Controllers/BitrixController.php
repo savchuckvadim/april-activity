68e76418b989e5fc2b92efa292a6cd6d5d92f4e1
savchuckvadim <savchuckvadim@gmail.com>
2024-02-02 12:25:55 +0100

    tst filter

--- app/Http/Controllers/BitrixController.php
+++ app/Http/Controllers/BitrixController.php
@@ -97,7 +97,7 @@ class BitrixController extends Controller
                     $cmdKey = "user_{$userId}_action_{$actionId}";
 
                     // Добавляем команду в массив команд
-                    $commands[$cmdKey] = "lists.element.get?IBLOCK_TYPE_ID=lists&IBLOCK_ID=86&FILTER[$userFieldId][0]=$userId&FILTER[$actionFieldId][0]=$actionId";
+                    $commands[$cmdKey] = "lists.element.get?IBLOCK_TYPE_ID=lists&IBLOCK_ID=86&filter[$userFieldId][0]=$userId";
 
                 }
             }
