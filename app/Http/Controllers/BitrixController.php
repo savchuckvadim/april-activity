33440d446a2941286d9a3ec9f334f9033269de2e
savchuckvadim <savchuckvadim@gmail.com>
2024-02-03 14:20:23 +0100

    tst date

--- app/Http/Controllers/BitrixController.php
+++ app/Http/Controllers/BitrixController.php
@@ -166,7 +166,7 @@ class BitrixController extends Controller
                     $cmdKey = "user_{$userId}_action_{$actionId}";
 
                     // Добавляем команду в массив команд
-                    $commands[$cmdKey] = "lists.element.get?IBLOCK_TYPE_ID=lists&IBLOCK_ID=86&filter[$userFieldId]=$userId&filter[$actionFieldId]=$actionId&filter[DATE_CREATE]=10.01.2024";
+                    $commands[$cmdKey] = "lists.element.get?IBLOCK_TYPE_ID=lists&IBLOCK_ID=86&filter[$userFieldId]=$userId&filter[$actionFieldId]=$actionId&filter[=DATE_CREATE]=10.01.2024";
                 }
             }
 
