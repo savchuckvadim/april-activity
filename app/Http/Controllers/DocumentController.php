f2e69c3b7c23a0ae29a6eb99f0a3ce3367e54979
savchuckvadim <savchuckvadim@gmail.com>
2024-02-19 15:38:35 +0100

    tst logo

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -1820,8 +1820,8 @@ class DocumentController extends Controller
 
         $tableHeader = $header->addTable();
         $tableHeader->addRow();
-        $headerRqWidth = $styles['page']['pageSizeW'] / 2.5;
-        $headerLogoWidth = $styles['page']['pageSizeW'] / 1.5;
+        $headerRqWidth = $styles['page']['pageSizeW'] * 0.4;
+        $headerLogoWidth = $styles['page']['pageSizeW'] * 0.6;
 
         $headerTextStyle = $styles['fonts']['text']['small'];
         $headerRqParagraf = $styles['paragraphs']['general'];
