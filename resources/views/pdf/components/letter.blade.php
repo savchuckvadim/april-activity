dae842873c0ae011d780a3b6b606b976b024037a
savchuckvadim <savchuckvadim@gmail.com>
2024-02-20 16:31:16 +0100

    fxd letter

--- resources/views/pdf/components/letter.blade.php
+++ resources/views/pdf/components/letter.blade.php
@@ -1,5 +1,51 @@
 <div class="letter">
-    <p class="text-normal">
-        Письмо
-    </p>
+    <div class="letter-rq">
+        <div class="letter-rq-left">
+            <p>
+                {{$letterData['documentNumber']}}
+            </p>
+        </div>
+
+        <div class="letter-rq-right">
+            @if ($headerData['companyName'] !== null)
+            <p>
+                {{$letterData['companyName']}}
+            </p>
+
+            @if ($headerData['inn'] !== null)
+            <p>
+                {{$letterData['inn']}}
+            </p>
+
+            @if ($headerData['positionCase'] !== null)
+            <p>
+                {{$letterData['positionCase']}}
+            </p>
+
+            @if ($headerData['recipientCase'] !== null)
+            <p>
+                {{$letterData['recipientCase']}}
+            </p>
+
+            @endif
+        </div>
+
+        <div class="letter-text">
+            @if ($headerData['recipientCase'] !== null)
+            <div class="letter-title-wrapper">
+                <h2>
+                    {{$letterData['recipientName']}}
+                </h2>
+            </div>
+            @endif
+
+            <div class="letter-title-wrapper">
+                <h2>
+                    {{$letterData['text']}}
+                </h2>
+            </div>
+        </div>
+
+    </div>
+
 </div>
