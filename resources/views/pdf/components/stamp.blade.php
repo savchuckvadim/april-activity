603f125b47b51ff3471706505e4021bc043a4e45
savchuckvadim <savchuckvadim@gmail.com>
2024-02-22 15:22:11 +0100

    fxd stamp

--- resources/views/pdf/components/stamp.blade.php
+++ resources/views/pdf/components/stamp.blade.php
@@ -17,10 +17,10 @@
     .signature-container img {
         position: absolute;
         left: 50%;
-      
+        top: 20px;
         /* Центрируем изображение по горизонтали */
         transform: translateX(-50%);
-    
+
     }
 
     /* Если нужно, чтобы печать была больше по размеру */
