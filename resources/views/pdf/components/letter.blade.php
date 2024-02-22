e3ff3f58a761e7f7905e9b2bebe2c0e0ee7614ad
savchuckvadim <savchuckvadim@gmail.com>
2024-02-22 15:43:34 +0100

    fxd letter margins

--- resources/views/pdf/components/letter.blade.php
+++ resources/views/pdf/components/letter.blade.php
@@ -1,9 +1,20 @@
+<style>
+    .letter {
+        margin-top: 50px;
+    }
+
+    .letter-text {
+        margin-top: 150px;
+    }
+</style>
+
+
 <div class="letter">
     <div class="row letter-rq">
         <div class="rowItem letter-rq-left">
             <div class="rq_wrapper">
                 <p class="text-small">
-                    {{$letterData['documentNumber']}}
+                    {{ $letterData['documentNumber'] }}
                 </p>
             </div>
 
@@ -11,60 +22,59 @@
         <div style="display: table-cell;"></div>
         <div class="rowItem letter-rq-right">
             @if ($letterData['companyName'] !== null)
-            <p class="text-small">
-                {{$letterData['companyName']}}
-            </p>
+                <p class="text-small">
+                    {{ $letterData['companyName'] }}
+                </p>
             @endif
             @if ($letterData['inn'] !== null)
-            <p class="text-small">
-                {{$letterData['inn']}}
-            </p>
+                <p class="text-small">
+                    {{ $letterData['inn'] }}
+                </p>
             @endif
             @if ($letterData['positionCase'] !== null)
-            <p class="text-small">
-                {{$letterData['positionCase']}}
-            </p>
+                <p class="text-small">
+                    {{ $letterData['positionCase'] }}
+                </p>
             @endif
             @if ($letterData['recipientCase'] !== null)
-            <p class="text-small">
-                {{$letterData['recipientCase']}}
-            </p>
-
+                <p class="text-small">
+                    {{ $letterData['recipientCase'] }}
+                </p>
             @endif
         </div>
 
     </div>
     <div class="letter-text">
         @if ($letterData['recipientCase'] !== null)
-        <div class="letter-title-wrapper">
-            <h2>
-                {{$letterData['recipientName']}}
-            </h2>
-        </div>
+            <div class="letter-title-wrapper">
+                <h2>
+                    {{ $letterData['recipientName'] }}
+                </h2>
+            </div>
         @endif
 
         <div class="letter-text-wrapper">
             @php
-            $letterText = $letterData['text'];
-            $parts = preg_split('/<color>|<\/color>/', $letterText);
-            $inHighlight = false;
+                $letterText = $letterData['text'];
+                $parts = preg_split('/<color>|<\/color>/', $letterText);
+                $inHighlight = false;
             @endphp
 
             @foreach ($parts as $index => $part)
-            @php
-            $isLastPart = $index === count($parts) - 1;
-            @endphp
+                @php
+                    $isLastPart = $index === count($parts) - 1;
+                @endphp
 
-            {{-- Замена \n на <br> и обертывание каждой части в span --}}
-            {!! $inHighlight ? '<span class="color text-normal">' : '<span class="text-normal">' !!}
-                    {!! nl2br(e($part)) !!}
+                {{-- Замена \n на <br> и обертывание каждой части в span --}}
+                {!! $inHighlight ? '<span class="color text-normal">' : '<span class="text-normal">' !!}
+                {!! nl2br(e($part)) !!}
                 </span>
 
                 {{-- Переключаем флаг выделения для следующей итерации --}}
                 @if (!$isLastPart)
-                @php $inHighlight = !$inHighlight @endphp
+                    @php $inHighlight = !$inHighlight @endphp
                 @endif
-                @endforeach
+            @endforeach
         </div>
     </div>
 </div>
