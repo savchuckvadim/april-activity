3af908fd8c397699114af26ef2812cb631e576b5
savchuckvadim <savchuckvadim@gmail.com>
2024-02-22 12:17:35 +0100

    fxd table

--- app/Http/Controllers/PDFDocumentController.php
+++ app/Http/Controllers/PDFDocumentController.php
@@ -361,7 +361,7 @@ class PDFDocumentController extends Controller
             if ($descriptionMode === 0) {
                 $itemsPerPage = 30;
             } else if ($descriptionMode === 1) {
-                $itemsPerPage = 16;
+                $itemsPerPage = 18;
             } else {
                 $itemsPerPage = 8;
             }
