93e60b35b5afa0648acd368a3f68980f02fb4ff9
savchuckvadim <savchuckvadim@gmail.com>
2024-02-23 11:18:43 +0100

    fxd colors

--- resources/views/pdf/offer.blade.php
+++ resources/views/pdf/offer.blade.php
@@ -72,8 +72,11 @@
 
         .color {
             font-weight: bold;
-            /* color: rgb(40, 104, 212); */
-            color: rgb(58, 102, 172);
+            /* color: rgb(40, 104, 212);  bitrix*/
+            /* color: rgb(58, 102, 172);  garant */
+            /* april */
+            color:rgb(69,193,238); 
+            
         }
 
         .red {
