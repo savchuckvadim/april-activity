30d45da2d5ca90721f1363b57cf1399fbfa9c7f6
savchuckvadim <savchuckvadim@gmail.com>
2024-02-23 16:56:27 +0100

    fxd mt

--- resources/views/pdf/components/invoice/top.blade.php
+++ resources/views/pdf/components/invoice/top.blade.php
@@ -43,7 +43,7 @@
                     </tr>
                 </table>
             </td>
-            <td rowspan="2" style="vertical-align: top;  height: 150px; width: 60mm;">
+            <td rowspan="2" style="vertical-align: top;  height: 100px; width: 60mm;">
                 <table class="innertable">
                     <tr class="innertable">
                         <td class="innertable">
@@ -53,7 +53,7 @@
                     </tr>
                     <tr class="innertable">
                         <td class="innertable">
-                            <p class="text-large" style="margin-top: 5px; line-height: 15mm; vertical-align: middle;">
+                            <p class="text-large" style="margin-top: 15px; line-height: 15mm; vertical-align: middle;">
                                 30101810000000000555</p>
                         </td>
                     </tr>
