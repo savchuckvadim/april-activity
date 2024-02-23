835939f7ee9cc52c456f1f88cb7a2d308c503299
savchuckvadim <savchuckvadim@gmail.com>
2024-02-23 14:39:54 +0100

    fxd top table

--- resources/views/pdf/components/invoice/top.blade.php
+++ resources/views/pdf/components/invoice/top.blade.php
@@ -22,37 +22,66 @@
 </style>
 
 <div class="invoice-top">
-    <table>
-        <tr class="first-row">
-            <td></td>
-            <td>
-                <table>
+    <table width="100%" border="2" style="border-collapse: collapse; width: 100%; font-family: Arial;" cellpadding="2" cellspacing="2">
+        <tr style="">
+            <td colspan="2" rowspan="2" style="min-height:13mm; width: 105mm;">
+                <table width="100%" border="0" cellpadding="0" cellspacing="0" style="height: 13mm;">
                     <tr>
-                        <td></td>
-
+                        <td valign="top">
+                            <div>СТ-ПЕТЕРБУРГСКИЙ филиал ПАО "ПРОМСВЯЗЬБАНК"<br>Г.</div>
+                        </td>
                     </tr>
                     <tr>
-                        <td></td>
-
+                        <td valign="bottom" style="height: 3mm;">
+                            <div style="font-size:10pt;">Банк получателя</div>
+                        </td>
                     </tr>
                 </table>
             </td>
-            <td>
-                <table>
-                    <tr>
-                        <td></td>
-
-                    </tr>
-                    <tr>
-                        <td></td>
-
-                    </tr>
-                </table>
+            <td style="min-height:7mm;height:auto; width: 25mm;">
+                <div>БИK</div>
+            </td>
+            <td rowspan="2" style="vertical-align: top; width: 60mm;">
+                <div style=" height: 7mm; line-height: 7mm; vertical-align: middle;">044030555</div>
+                <div>30101810000000000555</div>
             </td>
         </tr>
-
         <tr>
-
+            <td style="width: 25mm;">
+                <div>Сч. №</div>
+            </td>
+        </tr>
+        <tr>
+            <td style="min-height:6mm; height:auto; width: 50mm;">
+                <div>ИНН </div>
+            </td>
+            <td style="min-height:6mm; height:auto; width: 55mm;">
+                <div>КПП </div>
+            </td>
+            <td rowspan="2" style="min-height:19mm; height:auto; vertical-align: top; width: 25mm;">
+                <div>Сч. №</div>
+            </td>
+            <td rowspan="2" style="min-height:19mm; height:auto; vertical-align: top; width: 60mm;">
+                <div>40702810306000008712</div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="min-height:13mm; height:auto;">
+    
+                <table border="0" cellpadding="0" cellspacing="0" style="height: 13mm; width: 105mm;">
+                    <tr>
+                        <td valign="top">
+                            <div>ООО ""</div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td valign="bottom" style="height: 3mm;">
+                            <div style="font-size: 10pt;">Получатель</div>
+                        </td>
+                    </tr>
+                </table>
+    
+            </td>
         </tr>
     </table>
 
