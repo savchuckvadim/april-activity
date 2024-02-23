070279f7597aced1aace2bd9021e3adc0b807152
savchuckvadim <savchuckvadim@gmail.com>
2024-02-23 17:01:04 +0100

    fxd bik

--- resources/views/pdf/components/invoice/top.blade.php
+++ resources/views/pdf/components/invoice/top.blade.php
@@ -12,7 +12,8 @@
         border-collapse: collapse;
         border: 0px;
     }
-    .cellBottom{
+
+    .cellBottom {
         vertical-align: bottom;
     }
 </style>
@@ -51,73 +52,74 @@
                                 044030555</p>
                         </td>
                     </tr>
-                    <tr class="innertable" style="margin-top: 15px;">
-                        <td class="innertable">
-                            <p class="text-large" style="margin-top: 35px; line-height: 15mm; vertical-align: middle;">
-                                30101810000000000555</p>
-                        </td>
-                    </tr>
-                </table>
-
+                    <tr class="innertable" style="margin-top: 15px;"
+                         >
+                        <td class="innertable cellBottom" style="height: 100px;">
+                        <p class="text-large" style="margin-top: 35px; line-height: 15mm; vertical-align: middle;">
+                            30101810000000000555</p>
             </td>
         </tr>
-        <tr>
-            <td style="width: 25mm;">
-                <table class="innertable">
-                    <tr class="innertable">
-                        <td class="innertable">
-                            <p class="text-large">Сч. №</p>
-                        </td>
-                    </tr>
-                </table>
+    </table>
 
-            </td>
-        </tr>
-        <tr>
-            <td style="min-height:6mm; height:auto; width: 50mm;">
-                <p class="text-large">ИНН </p>
-            </td>
-            <td style="min-height:6mm; height:auto; width: 55mm;">
-                <p class="text-large">КПП </p>
-            </td>
-            <td rowspan="2" style="min-height:19mm; height:auto; vertical-align: top; width: 25mm;">
-                <table class="innertable">
-                    <tr class="innertable">
-                        <td class="innertable">
-                            <p class="text-large">Сч. №</p>
-                        </td>
-                    </tr>
-                </table>
+    </td>
+    </tr>
+    <tr>
+        <td style="width: 25mm;">
+            <table class="innertable">
+                <tr class="innertable">
+                    <td class="innertable">
+                        <p class="text-large">Сч. №</p>
+                    </td>
+                </tr>
+            </table>
 
-            </td>
-            <td rowspan="2" style="min-height:19mm; height:auto; vertical-align: top; width: 60mm;">
-                <table class="innertable">
-                    <tr class="innertable">
-                        <td class="innertable">
-                            <p class="text-large">40702810306000008712</p>
-                        </td>
-                    </tr>
-                </table>
+        </td>
+    </tr>
+    <tr>
+        <td style="min-height:6mm; height:auto; width: 50mm;">
+            <p class="text-large">ИНН </p>
+        </td>
+        <td style="min-height:6mm; height:auto; width: 55mm;">
+            <p class="text-large">КПП </p>
+        </td>
+        <td rowspan="2" style="min-height:19mm; height:auto; vertical-align: top; width: 25mm;">
+            <table class="innertable">
+                <tr class="innertable">
+                    <td class="innertable">
+                        <p class="text-large">Сч. №</p>
+                    </td>
+                </tr>
+            </table>
 
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2" style="min-height:13mm; height:auto;">
+        </td>
+        <td rowspan="2" style="min-height:19mm; height:auto; vertical-align: top; width: 60mm;">
+            <table class="innertable">
+                <tr class="innertable">
+                    <td class="innertable">
+                        <p class="text-large">40702810306000008712</p>
+                    </td>
+                </tr>
+            </table>
 
-                <table class="innertable" cellpadding="0" cellspacing="0" style="height: 23mm; width: 105mm;">
-                    <tr class="innertable">
-                        <td class="innertable" valign="top">
-                            <p class="text-large">ООО ""</p>
-                        </td>
-                    </tr>
-                    <tr class="innertable">
-                        <td class="innertable" valign="bottom" style="height: 3mm;">
-                            <p class="text-large">Получатель</p>
-                        </td>
-                    </tr>
-                </table>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" style="min-height:13mm; height:auto;">
 
-            </td>
-        </tr>
+            <table class="innertable" cellpadding="0" cellspacing="0" style="height: 23mm; width: 105mm;">
+                <tr class="innertable">
+                    <td class="innertable" valign="top">
+                        <p class="text-large">ООО ""</p>
+                    </td>
+                </tr>
+                <tr class="innertable">
+                    <td class="innertable" valign="bottom" style="height: 3mm;">
+                        <p class="text-large">Получатель</p>
+                    </td>
+                </tr>
+            </table>
+
+        </td>
+    </tr>
     </table>
 </div>
