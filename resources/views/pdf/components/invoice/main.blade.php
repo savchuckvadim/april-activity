9045ddfbbd8c5cd1e5bbbb55eb6ae22109a950d4
savchuckvadim <savchuckvadim@gmail.com>
2024-02-29 09:54:23 +0100

    fxd main

--- resources/views/pdf/components/invoice/main.blade.php
+++ resources/views/pdf/components/invoice/main.blade.php
@@ -20,4 +20,27 @@
 
     <h3 class="invoice-title">{{ $number }}</h3>
 
+    <table>
+        <tr>
+            <td>
+                <p>Поставщик: {{ $rq['fullname'] }}, ИНН: {{ $rq['inn'] }}, {{ $rq['registredAdress'] }},
+                    {{ $rq['phone'] }}</p>
+            </td>
+        </tr>
+        @if ($recipient['companyName'])
+            @php
+                $inn = null;
+                if ($recipient['inn']) {
+                    $inn = ', ИНН: ' . $recipient['inn'];
+                }
+
+            @endphp
+            <tr>
+                <td>
+                    <p>Покупатель: {{ $recipient['companyName'] }}{{ $inn }} </p>
+                </td>
+            </tr>
+        @endif
+
+    </table>
 </div>
