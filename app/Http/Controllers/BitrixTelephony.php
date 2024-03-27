b150cda467351425d731a3332562bd55daee9480
savchuckvadim <savchuckvadim@gmail.com>
2024-03-27 19:59:46 +0700

    fxd request

--- app/Http/Controllers/BitrixTelephony.php
+++ app/Http/Controllers/BitrixTelephony.php
@@ -10,9 +10,9 @@ class BitrixTelephony extends Controller
     public function statisticGet(Request $request){
 
         $domain = 'april-garant.bitrix24.ru';
-        $method = '/crm.activity.configurable.add.json';
+        // $method = '/crm.activity.configurable.add.json';
         // $hook = env('TEST_HOOK');
-        $hook = BitrixController::getHook($domain);
-        Log::info('Звонок: ', ['$request' => $request->body()] );
+        // $hook = BitrixController::getHook($domain);
+        Log::info('Звонок: ', ['$request' => $request->json()] );
     }
 }
