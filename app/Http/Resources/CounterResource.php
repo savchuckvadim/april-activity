bbdec6663926ecd9e692017542318a380c3f49eb
savchuckvadim <savchuckvadim@gmail.com>
2024-04-01 20:04:50 +0700

    return resource

--- app/Http/Resources/CounterResource.php
+++ app/Http/Resources/CounterResource.php
@@ -14,30 +14,18 @@ class CounterResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        // Предполагаем, что 'rqs' - это название отношения многие ко многим
         return [
             'id' => $this->id,
-            'rqs' => $this->whenLoaded('rqs', function () {
-                // Возвращаем данные из pivot, если связь 'rqs' загружена
-                return $this->rqs->map(function ($rq) {
-                    return [
-                        'id' => $rq->id,
-                        'name' => $rq->name,
-                        // Пример возвращения данных pivot
-                        'pivot' => [
-                            'value' => $rq->pivot->value,
-                            'type' => $rq->pivot->type,
-                            'prefix' => $rq->pivot->prefix,
-                            'postfix' => $rq->pivot->postfix,
-                            'day' => $rq->pivot->day,
-                            'year' => $rq->pivot->year,
-                            'month' => $rq->pivot->month,
-                            'count' => $rq->pivot->count,
-                            'size' => $rq->pivot->size,
-                        ],
-                    ];
-                });
-            }),
+            'value' => $this->pivot ? $this->pivot->value : null,
+            'type' => $this->pivot ? $this->pivot->type : null,
+            'prefix' => $this->pivot ? $this->pivot->prefix : null,
+            'postfix' => $this->pivot ? $this->pivot->postfix : null,
+            'day' => $this->pivot ? $this->pivot->day : null,
+            'year' => $this->pivot ? $this->pivot->year : null,
+            'month' => $this->pivot ? $this->pivot->month : null,
+            'count' => $this->pivot ? $this->pivot->count : null,
+            'size' => $this->pivot ? $this->pivot->size : null,
+            // Добавьте другие поля, которые вам нужны
         ];
     }
 }
