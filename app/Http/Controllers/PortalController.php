794e336d928303d65e5ba858f60d2a8d3d0e9035
savchuckvadim <savchuckvadim@gmail.com>
2024-04-29 21:48:43 +0700

    fxd portal

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -76,14 +76,14 @@ class PortalController extends Controller
         $cacheKey = 'portal_' . $domain;
         $cachedPortalData = Cache::get($cacheKey);
 
-        if (!is_null($cachedPortalData)) {
-            Log::channel('telegram')->info('APRIL_ONLINE', [
-                'log from cache getPortal'   =>
-                'cachedPortal'
+        // if (!is_null($cachedPortalData)) {
+        //     Log::channel('telegram')->info('APRIL_ONLINE', [
+        //         'log from cache getPortal'   =>
+        //         'cachedPortal'
 
-            ]);
-            return APIController::getSuccess(['portal' => $cachedPortalData]); // Возвращаем данные в формате response
-        }
+        //     ]);
+        //     return APIController::getSuccess(['portal' => $cachedPortalData]); // Возвращаем данные в формате response
+        // }
 
         $portal = Portal::where('domain', $domain)->first();
         if (!$portal) {
@@ -105,9 +105,9 @@ class PortalController extends Controller
             'bitrixList' => $portal->getSalesBitrixListId(),
             'bitrixCallingTasksGroup' => $portal->getSalesCallingGroupId(),
             'bitrixSmart' => $portal->getSalesSmart(),
-            // 'deal' => $portal->deal(), // Убедитесь, что здесь нужны только ID или базовые данные
-            // 'company' => $portal->company(),
-            // 'lead' => $portal->lead(),
+            'deal' => $portal->deal(), // Убедитесь, что здесь нужны только ID или базовые данные
+            'company' => $portal->company(),
+            'lead' => $portal->lead(),
         ];
 
         Cache::put($cacheKey, $portalData, now()->addMinutes(10)); // Кешируем данные портала
