21312da555448dfc830ee2283e58c097232ebfc2
savchuckvadim <savchuckvadim@gmail.com>
2024-04-29 17:30:08 +0700

    fxs migration

---
+++ database/migrations/2024_04_29_102938_add_portal_id_to_btx_leads.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('btx_leads', function (Blueprint $table) {
+            $table->unsignedBigInteger('portal_id')->after('code'); // Добавить после столбца 'code'
+            $table->foreign('portal_id')->references('id')->on('portals')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('btx_leads', function (Blueprint $table) {
+            $table->dropForeign(['portal_id']);
+            $table->dropColumn('portal_id');
+        });
+    }
+};
