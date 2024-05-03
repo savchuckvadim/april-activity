64a297e81f707c2f619953048f5bc52531a31ad2
savchuckvadim <savchuckvadim@gmail.com>
2024-05-03 13:27:55 +0700

    add logs

--- app/Http/Controllers/DealController.php
+++ app/Http/Controllers/DealController.php
@@ -38,20 +38,28 @@ class DealController extends Controller
             if(isset($request->regions)){
                 $deal['regions'] = $request->regions;
 
-                Log::channel('telegram')->info('APRIL_ONLINE', [
-                    'regions' => [
+                // Log::channel('telegram')->info('APRIL_ONLINE', [
+                //     'regions' => [
     
-                        'regions' => $request->regions,
+                //         'regions' => $request->regions,
                        
     
     
-                    ]
-                ]);
+                //     ]
+                // ]);
 
 
 
             }
-            
+            Log::channel('telegram')->info('APRIL_ONLINE', [
+                'regions' => [
+
+                    'deal' => $deal,
+                   
+
+
+                ]
+            ]);
             $resultDeal = null;
             $resultCode = 1;
             $message = 'something wrong with saving deal';
