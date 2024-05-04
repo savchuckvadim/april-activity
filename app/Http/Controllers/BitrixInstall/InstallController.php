c725df4ecccb1cf85e463ec34d002c10b6b9c37e
savchuckvadim <savchuckvadim@gmail.com>
2024-05-04 17:59:29 +0700

    fxd admin cors

---
+++ app/Http/Controllers/BitrixInstall/InstallController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\BitrixInstall;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InstallController extends Controller
+{
+    public function fields()
+    {
+    }
+}
