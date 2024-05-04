5f65eb1fa882d24d8c22aa27cfc5df5718255480
savchuckvadim <savchuckvadim@gmail.com>
2024-05-04 15:43:03 +0700

    add admin hendle cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,6 +48,14 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             // 'verifed'
         ],
+        'admin' => [
+            // \Fruitcake\Cors\HandleCors::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // 'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // 'verifed'
+        ],
     ];
 
     /**
