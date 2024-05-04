b072105290a67580f66d8cdbba0a6b2b7b32192a
savchuckvadim <savchuckvadim@gmail.com>
2024-05-04 18:26:20 +0700

    fxd service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -42,7 +42,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
-            Route::prefix('api/admin')
+            Route::prefix('admin')
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/admin.php'));
