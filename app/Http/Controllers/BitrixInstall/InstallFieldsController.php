c540526019a0c33a9213d47969d82ad8e80d5ade
savchuckvadim <savchuckvadim@gmail.com>
2024-05-07 21:55:20 +0700

    fxd logs

--- app/Http/Controllers/BitrixInstall/InstallFieldsController.php
+++ app/Http/Controllers/BitrixInstall/InstallFieldsController.php
@@ -142,12 +142,12 @@ class InstallFieldsController extends Controller
             // $webhookRestKey = $portal['portal']['C_REST_WEB_HOOK_URL'];
             $hook = 'https://' . $domain . '/' . $webhookRestKey;
 
-            Log::channel('telegram')->info('APRIL_ONLINE TEST', ['INSTALL' => [
-                'portalDealFields' => $portalDealFields,
-                'portalLeadFields' => $portalLeadFields,
-                'portalCompanyFields' => $portalCompanyFields,
-                'portalsmarts' => $portalsmarts,
-            ]]);
+            // Log::channel('telegram')->info('APRIL_ONLINE TEST', ['INSTALL' => [
+            //     'portalDealFields' => $portalDealFields,
+            //     'portalLeadFields' => $portalLeadFields,
+            //     'portalCompanyFields' => $portalCompanyFields,
+            //     'portalsmarts' => $portalsmarts,
+            // ]]);
             // $methodSmartInstall = '/crm.type.add.json';
             // $url = $hook . $methodSmartInstall;
 
@@ -409,10 +409,10 @@ class InstallFieldsController extends Controller
                     "SETTINGS" => ["LIST_HEIGHT" => 1],
                     "ORDER" => 134,
                 ];
-                Log::channel('telegram')->error("fieldsData", [
-                    'fieldsData' => $fieldsData,
+                // Log::channel('telegram')->error("fieldsData", [
+                //     'fieldsData' => $fieldsData,
 
-                ]);
+                // ]);
                 $data = [
                     'fields' => $fieldsData
                 ];
