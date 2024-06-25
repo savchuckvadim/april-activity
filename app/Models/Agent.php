ddda4f50d18e188ebcfc869d9a53aa283aa3f0f3
savchuckvadim <savchuckvadim@gmail.com>
2024-06-25 09:13:56 +0300

    add with rq

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class Agent extends Model
 {
     use HasFactory;
+    protected $with = ['rq']; 
     protected $fillable = [
         'number', 'type', 
         'portalId', 'name', 'code'
