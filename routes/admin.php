1449a27eb6507fca7e6a50747dd457d329feffb4
savchuckvadim <savchuckvadim@gmail.com>
2024-06-25 10:45:52 +0300

    fxd middlwe

--- routes/admin.php
+++ routes/admin.php
@@ -62,7 +62,7 @@ use Illuminate\Support\Facades\Route;
 
 // Route::middleware(['ajax.only'])->group(function () {
 
-Route::middleware([])->group(function () {
+Route::middleware(['ajax.only'])->group(function () {
 
 
 
