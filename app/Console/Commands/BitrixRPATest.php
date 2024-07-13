ced3e64d5676faca4deb62a2e47f138df29a837f
savchuckvadim <savchuckvadim@gmail.com>
2024-07-13 16:50:04 +0300

    fxd BitrixRPATestBitrixRPATest

--- app/Console/Commands/BitrixRPATest.php
+++ app/Console/Commands/BitrixRPATest.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\BitrixController;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
-class BitrixInstallTest extends Command
+class BitrixRPATest extends Command
 {
     /**
      * The name and signature of the console command.
