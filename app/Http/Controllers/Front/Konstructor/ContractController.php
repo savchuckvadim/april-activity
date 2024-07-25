fb428931277e6b5428b4fc7fb1f1130040e54f25
savchuckvadim <savchuckvadim@gmail.com>
2024-07-25 10:50:28 +0300

    add portal contracts

--- app/Http/Controllers/Front/Konstructor/ContractController.php
+++ app/Http/Controllers/Front/Konstructor/ContractController.php
@@ -10,12 +10,8 @@ use Illuminate\Http\Request;
 
 class ContractController extends Controller
 {
-  
 
-  
-    /**
-     * Display the specified resource.
-     */
+
     public function get($portalContractId) //by id
     {
         try {
@@ -58,46 +54,25 @@ class ContractController extends Controller
         );
     }
 
-
-
-    // public function getByDomain($domain)
-    // {
-    //     $portalcontracts = [];
-    //     // Создание нового Counter
-    //     $portal = Portal::find($portalId);
-    //     if ($portal) {
-    //         $portalcontracts = $portal->portalcontracts;
-    //         if ($portalcontracts) {
-
-    //             return APIController::getSuccess(
-    //                 ['portalcontracts' => $portalcontracts]
-    //             );
-    //         }
-    //     }
-
-
-    //     return APIController::getSuccess(
-    //         ['portalcontracts' => $portalcontracts]
-    //     );
-    // }
-    /**
-     * Show the form for editing the specified resource.
-     */
-
-    public function destroy($portalcontractId)
+    public function getByPortal(Request $request)
     {
-        $portalcontract = PortalContract::find($portalcontractId);
+        $domain =  $request->domain;
+        $portalcontracts = [];
+        // Создание нового Counter
+        $portal = Portal::where('domain', $domain)->first();
+        if ($portal) {
+            $portalcontracts = $portal->portalcontracts;
+            if ($portalcontracts) {
 
-        if ($portalcontract) {
-            // Получаем все связанные поля
-            $portalcontract->delete();
-            return APIController::getSuccess($portalcontract);
-        } else {
-
-            return APIController::getError(
-                'portalcontract not found',
-                null
-            );
+                return APIController::getSuccess(
+                    ['contracts' => $portalcontracts]
+                );
+            }
         }
+
+
+        return APIController::getSuccess(
+            ['portalcontracts' => $portalcontracts]
+        );
     }
 }
---
+++ routes/front/konstructor/contract_routes.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\Front\Konstructor\ContractController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('konstruct')->group(function () {
+   
+    Route::prefix('contract')->group(function () {
+   
+        
+    });
+
+    Route::prefix('contracts')->group(function () {
+   
+        Route::post('/', [ContractController::class, 'getByPortal']);
+    
+    });
+});
+
+
+
+
+// ......................................................................... SMARTS
+
+//.................................... initial RPA
+// initial from parent
+// Route::get('initial/portal/{portalId}/measure', [MeasureController::class, 'getInitial']);
+// single initial
+Route::get('initial/contract',  [ContractController::class, 'getInitial']);
+// .............................................GET  SMART
+// all from parent  smart
+// Route::get('measures', [MeasureController::class, 'getAll']);
+
+// ...............  get smart
+// Route::get('measure/{measureId}', [MeasureController::class, 'get']);
+
+
+//...............................................SET RPA
+
+// Route::post('measure', [MeasureController::class, 'store']);
+// Route::post('measure/{measureId}', [MeasureController::class, 'store']);
+
+// ............................................DELETE
+// Route::delete('measure/{measureaId}', [MeasureController::class, 'destroy']);
+
