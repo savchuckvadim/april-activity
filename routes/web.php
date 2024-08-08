540423fba2beee6071372877a5e022e2a5387afe
savchuckvadim <savchuckvadim@gmail.com>
2024-08-08 10:35:47 +0300

    add install list

--- routes/web.php
+++ routes/web.php
@@ -79,11 +79,11 @@ Route::get('/install/lists/{pass}/{domain}/{token}/', function ($pass, $domain, 
     //     'token' => $token,
     // ]);
 
-    // if ($pass == 'nmbrsdntl' && $domain) {
-    //     return ListController::setLists($token, $domain);
-    // } else {
+    if ($pass == 'nmbrsdntl' && $domain) {
+        return ListController::setLists($token, $domain);
+    } else {
     return 'yo';
-    // }
+    }
 });
 
 
