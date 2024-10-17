6938ddd22dfceb3f69e70d5c899962f199278f62
savchuckvadim <savchuckvadim@gmail.com>
2024-10-17 14:07:17 +0200

    without key

--- routes/client_outer/client.php
+++ routes/client_outer/client.php
@@ -1,12 +1,12 @@
 <?php
 
 
-use App\Http\Controllers\AdminOuter\ListController;
 use App\Http\Controllers\PortalController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::prefix('client')->middleware('check.ip.api_key')->group(function () {
+// Route::prefix('client')->middleware('check.ip.api_key')->group(function () {
+    Route::prefix('client')->group(function () {
 
     Route::post('portal', function (Request $request) {
         $domain  = $request->input('domain');
