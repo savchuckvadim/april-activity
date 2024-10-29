6959043fb90bc3cbff2fed71618f471c983d4409
savchuckvadim <savchuckvadim@gmail.com>
2024-10-29 14:14:57 +0200

    opaen install

--- routes/client_outer/client.php
+++ routes/client_outer/client.php
@@ -9,9 +9,9 @@ Route::prefix('client')->middleware('check.ip.api_key')->group(function () {
 
     Route::post('portal', function (Request $request) {
         $domain  = $request->input('domain');
-        // if ($domain == 'b24-683ezu.bitrix24.ru' || $domain == 'april-dev.bitrix24.ru') {
-        //     return PortalController::getFrontPortal($domain);
-        // }
+        if ($domain == 'b24-683ezu.bitrix24.ru' || $domain == 'april-dev.bitrix24.ru') {
+            return PortalController::getFrontPortal($domain);
+        }
 
         return APIController::getError('portal was not found or permissions что-то там', [
             'domain' => $domain
