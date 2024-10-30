afd8d5a9f8aaa12aeeda46e52cb07c314e8c88ae
savchuckvadim <savchuckvadim@gmail.com>
2024-10-30 08:12:18 +0200

    add instal portal test

--- routes/client_outer/client.php
+++ routes/client_outer/client.php
@@ -9,7 +9,7 @@ Route::prefix('client')->middleware('check.ip.api_key')->group(function () {
 
     Route::post('portal', function (Request $request) {
         $domain  = $request->input('domain');
-        if ($domain == 'b24-683ezu.bitrix24.ru' || $domain == 'april-dev.bitrix24.ru') {
+        if ($domain == 'b24-683ezu.bitrix24.ru' || $domain == 'april-dev.bitrix24.ru' || $domain ==  'b24-20qnxm.bitrix24.ru') {
             return PortalController::getFrontPortal($domain);
         }
 
