cfb19f09e509c238d28446c25101524ad77031a2
savchuckvadim <savchuckvadim@gmail.com>
2024-11-29 15:05:12 +0200

    fxd product dto

--- app/DTO/DocumentContract/DocumentContractDataDTO.php
+++ app/DTO/DocumentContract/DocumentContractDataDTO.php
@@ -35,7 +35,10 @@ class DocumentContractDataDTO
             contractType: $request->input('contractType'),
             supply: SupplyDTO::fromArray($request->input('supply')),
             contract: ContractDTO::fromArray($request->input('contract')),
-            products: array_map(fn($product) => !empty($product) ?? ProductDTO::fromArray($product), $request->input('products', [])),
+            products: array_map(
+                fn($product) => !empty($product) ? ProductDTO::fromArray($product) : null,
+                $request->input('products', [])
+            ),
             arows: array_map(fn($row) => RowDTO::fromArray($row), $request->input('arows', [])),
             contractBaseState: $request->input('contractBaseState', []),
             contractClientState: $request->input('contractClientState', []),
