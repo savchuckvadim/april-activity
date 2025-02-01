1dad3deedaa84cc8c1a4faf718e7b03fa19bdabb
savchuckvadim <savchuckvadim@gmail.com>
2025-02-02 03:35:01 +0800

    sdd complect route

--- routes/admin/garant/complect_routes.php
+++ routes/admin/garant/complect_routes.php
@@ -10,7 +10,7 @@ Route::prefix('')->group(function () {
     Route::get('initial/complect', [ComplectController::class, 'getInitial']);
     // Route::get('initial', [ComplectController::class, 'getInitial']);
     // Route::get('{smartId}', [SmartController::class, 'get']);
-    // Route::post('{portalId}', [SmartController::class, 'store']);
+    Route::post('complect', [ComplectController::class, 'store']);
     // Route::delete('{smartId}', [SmartController::class, 'delete']);
 });
 
