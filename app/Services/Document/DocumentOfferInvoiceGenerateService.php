372733b029ce9cc5d64725a85494b5be7c4b1637
savchuckvadim <savchuckvadim@gmail.com>
2025-02-12 13:31:11 +0300

    tst sale text

--- app/Services/Document/DocumentOfferInvoiceGenerateService.php
+++ app/Services/Document/DocumentOfferInvoiceGenerateService.php
@@ -85,13 +85,15 @@ class DocumentOfferInvoiceGenerateService
         // Условная вставка блоков
         if ($withPrice) {
             // Вставляем блок с ценой
-            $processor->cloneBlock('if_letterWithPrice', 0, true, false);
+            $processor->cloneBlock('if_letterWithPrice', 0, true, false, );
             $processor->deleteBlock('if_letterWithoutPrice');
         } else {
             // Вставляем блок без цены
             $processor->cloneBlock('if_letterWithoutPrice', 0, true, false);
             $processor->deleteBlock('if_letterWithPrice');
         }
+        $processor->setValue('sale_text', 'Скидка 20% на весь ассортимент!');
+
     }
     protected function processInfoblocks()
     {
