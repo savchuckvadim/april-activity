057f35be10f508ca6f56297a05fbda3a11851a33
savchuckvadim <savchuckvadim@gmail.com>
2025-02-17 14:06:25 +0300

    fxd alternative

--- app/Http/Controllers/Front/Konstructor/FavoriteController.php
+++ app/Http/Controllers/Front/Konstructor/FavoriteController.php
@@ -255,7 +255,10 @@ class FavoriteController extends Controller
         }
         foreach ($alternatives as $alternative) {
             $alt = new RowDTO($alternative['total'][0]);
-            $comprasions = $alt->name.' • '.$alt->product->supplyName.' • '.$alt->product->contract->shortName;
+            $comprasions = $alt->name.' • '
+            .$alt->product->supplyName
+            .' • Договор: '.$alt->product->contract->aprilName
+            .' • Единица: '.$alt->product->measureFullName;
         }
         return [
             'id' => $favorite->id,
