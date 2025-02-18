9c8d137d741aaafbc70700ec80a9542be1eea3d5
savchuckvadim <savchuckvadim@gmail.com>
2025-02-18 15:59:41 +0300

    fxd create default zakupki offer

--- app/Http/Controllers/Front/Konstructor/OfferZakupkiSettingsController.php
+++ app/Http/Controllers/Front/Konstructor/OfferZakupkiSettingsController.php
@@ -16,11 +16,16 @@ class OfferZakupkiSettingsController extends Controller
         try {
             $portal = Portal::where('domain', $domain)->first();
             $settings = OfferZakupkiSettings::where('domain', $domain)
-                // ->where('userId', $userId)
+                ->where('userId', $userId)
                 ->first();
-        
 
-            
+            if (!$settings) {
+                $settings = OfferZakupkiSettings::where('domain', $domain)
+                    ->first();
+            }
+            if (!$settings) {
+                $settings = $this->createDefaultSettings($domain);
+            }
             $result = [
                 'settings' => $settings
             ];
@@ -40,5 +45,23 @@ class OfferZakupkiSettingsController extends Controller
 
             ]);
         }
+    }
+
+    protected function createDefaultSettings($domain)
+    {
+        $portal = Portal::where('domain', $domain)->first();
+        $data = array(
+            'portal_id' => $portal->id,
+            'domain' => $domain,
+            'name' => 'Настройки по умолчанию',
+            'provider1_letter_text' => 'Имеем честь предложить заключить контракт на период {{period}} на следующий комплект: ',
+
+            'provider2_letter_text' => 'Имеем честь предложить заключить контракт на период с  {{period}}  года на следующий комплект: ',
+            'is_default' => true,
+
+        );
+        $settings = new OfferZakupkiSettings($data);
+        $settings->save();
+        return $settings;
     }
 }
