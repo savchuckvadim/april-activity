d698e0710ac257fc41d33d9cfc9e4405e27e2d6d
savchuckvadim <savchuckvadim@gmail.com>
2025-02-19 15:50:08 +0300

    fxd to file

--- app/Console/Commands/BitrixTelephonyTest.php
+++ app/Console/Commands/BitrixTelephonyTest.php
@@ -82,16 +82,22 @@ class BitrixTelephonyTest extends Command
                     $this->line(json_encode($fileId));
                     $this->line(json_encode('url'));
                     $this->line(json_encode($fileUrl));
-
+                    $getFileMethod = '/disk.file.get';
+                    $url = $hook . $getFileMethod;
+                    $responseFile = Http::get($url, [
+                        'id' => $fileId
+                    ]);
+                    $this->line(json_encode('responseFile'));
+                    $this->line(json_encode($responseFile));
                     // Здесь ваш код для обработки URL файла
 
-                    $hook = BitrixController::getHook($domain); // Это твой вебхук
-                    $fileUrlWithHook = $fileUrl . '&' . parse_url($hook, PHP_URL_QUERY);
-                    $response = Http::withOptions(['allow_redirects' => true])->get($fileUrlWithHook);
+                    // $hook = BitrixController::getHook($domain); // Это твой вебхук
+                    // $fileUrlWithHook = $fileUrl . '&' . parse_url($hook, PHP_URL_QUERY);
+                    // $response = Http::withOptions(['allow_redirects' => true])->get($fileUrlWithHook);
 
-                    $this->line("Status Code: " . $response->status());
-                    $this->line("Headers: " . json_encode($response->headers()));
-                    $this->line("Body: " . substr($response->body(), 0, 500));
+                    // $this->line("Status Code: " . $response->status());
+                    // $this->line("Headers: " . json_encode($response->headers()));
+                    // $this->line("Body: " . substr($response->body(), 0, 500));
 
 
                     $fileContent = Http::get($fileUrl);
