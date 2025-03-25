af9ba1b4cc1ddc8c0cf8159b4c5963d01808e78d
savchuckvadim <savchuckvadim@gmail.com>
2025-03-26 01:43:59 +0800

    add with tax

---
+++ database/migrations/2025_03_25_174106_add_columns_to_agents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('agents', function (Blueprint $table) {
+            $table->boolean('withTax')->default(false);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('agents', function (Blueprint $table) {
+            $table->dropColum('withTax'); // Откат изменений
+
+        });
+    }
+};
